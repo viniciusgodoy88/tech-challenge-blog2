47560ac57605760b4e7a31c245de8d616a4ae95c
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-07-03 22:36:00 -0300

    fix: resolve Jest module mapping, controller paths, and CI test failures

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,9 +5,9 @@ const postController = require("../controllers/postController");
 
 router.post("/", (req, res) => postController.create(req, res));
 router.get("/", (req, res) => postController.findAll(req, res));
-router.get("/search", (req, res) => postController.search(req, res));
 router.get("/:id", (req, res) => postController.findById(req, res));
 router.put("/:id", (req, res) => postController.update(req, res));
 router.delete("/:id", (req, res) => postController.delete(req, res));
+router.get("/search", (req, res) => postController.search(req, res));
 
 module.exports = router;
